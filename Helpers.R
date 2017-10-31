a631e9f2609b0a8d6779f37a67e27056aa037746
Spugnam <quentin.picard@gmail.com>
2017-10-31 17:21:43 -0400

    added helpers, updated missingness

---
+++ Helpers.R
@@ -0,0 +1,27 @@
+#----------------------------------------- Functions ---------------------------
+
+NA.analysis <- function(df)
+{
+  # prints the number of NA's and the % of overall values for all df columns containing at least one
+  NA.count <- sapply(df, function(x) sum(is.na(x)))
+  NA.percentage <- sapply(df, function(x) 100*sum(is.na(x))/length(x))
+  
+  #Create result matrix
+  NA.summary = cbind(NA.count[NA.count>0], NA.percentage[NA.count>0])
+  colnames(NA.summary) <- c("Count", "Percent")
+  
+  return(NA.summary[order(desc(NA.summary[,"Percent"])),])
+}
+
+
+
+replace.NA.most.frequent.value <- function(df, column) {
+  # Finds the most frequent value and replaces all NA's with it
+  t <-table(as.factor(df[,column]), exclude=NULL)
+  df[[column]][is.na(df[[column]])] <- names(which.max(t))
+  return(df[[column]])
+}
+
+
+
+
