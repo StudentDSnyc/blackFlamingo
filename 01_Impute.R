110f2d8fb586f61d885a30c3b5a57ba47f9f716d
= <paul.ton@outlook.com>
2017-11-01 02:01:13 -0400

    impute pass 2

--- 01_Impute.R
+++ 01_Impute.R
@@ -29,6 +29,37 @@ for (column in to.impute){
   houses.test[[column]] <- impute.mode(houses.test, column) 
 }
 
+# Impute LotFrontage from LotArea
+
+# Possible imputation strategies
+houses.all <- rbind(houses.train, houses.test)
+impute.linear1 <- lm(LotFrontage ~ LotArea, data = houses.all)
+impute.linear2 <- lm(LotFrontage ~ 0 + LotArea, data = houses.all)
+impute.log1 <- lm(LotFrontage ~ log(LotArea), data = houses.all)
+impute.log2 <- lm(LotFrontage ~ 0 + log(LotArea), data = houses.all)
+
+# Justification for picking impute.log2:
+# In the physical world, area is proportional to length squared
+# Also, when the area is 0, the length should be 0
+
+# Impute train using log2 model
+imputed <- predict(impute.log2, houses.train)
+houses.train$LotFrontage[is.na(houses.train$LotFrontage)] <- imputed[is.na(houses.train$LotFrontage)]
+
+# Impute test using log2 model
+imputed <- predict(impute.log2, houses.test)
+houses.test$LotFrontage[is.na(houses.test$LotFrontage)] <- imputed[is.na(houses.test$LotFrontage)]
+
+# Impute GarageYrBlt using YearBuilt
+houses.train$GarageYrBlt[is.na(houses.train$GarageYrBlt)] <- 
+  houses.train$YearBuilt[is.na(houses.train$GarageYrBlt)]
+
+houses.test$GarageYrBlt[is.na(houses.test$GarageYrBlt)] <- 
+  houses.test$YearBuilt[is.na(houses.test$GarageYrBlt)]
+
+# Fix Outlier
+houses.test$GarageYrBlt[which(houses.test$GarageYrBlt == 2207)] <- 2007
+
 # Re-check
 NA.analysis(rbind(houses.train, houses.test))
 
--- 02_Baseline.R
+++ 02_Baseline.R
@@ -8,3 +8,26 @@ source("Helpers.R")
 
 # Load Data
 load("./house_imputed.RData")
+
+# Split our data into a train and test set - 80/20
+set.seed(0)
+train = sample(1:nrow(houses.train), 8*nrow(houses.train)/10)
+
+private.train = houses.train[train,]
+private.test = houses.train[-train,]
+
+# Baseline linear model
+houses.train$SalePrice <- log(houses.train$SalePrice + 1)
+model.baseline <- lm(SalePrice ~ ., data=private.train)
+
+plot(model.baseline)
+
+# RMSE of model on our test split
+private.test[which(private.test$Condition2 %in% c("PosA", "RRNn"))]$Condition2 <- "Norm"
+private.test[which(private.test$Exterior1st %in% c("ImStucc"))]$Exterior1st <- "VinylSd"
+## Not sure how to handle missing factor levels in test set
+
+predicted <- predict(model.baseline, private.test, na.action = na.exclude)
+actual <- houses.train[test,]$SalePrice
+
+sqrt(mean((predicted-actual)^2))
