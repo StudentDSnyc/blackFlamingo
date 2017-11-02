0c708e790f6be6c00db1b11f6a720b92ddf9d4fd
Spugnam <quentin.picard@gmail.com>
2017-11-02 14:04:49 -0400

    LR baseline with scale/ One-hot encoding

--- 02_Baseline.R
+++ 02_Baseline.R
@@ -9,42 +9,66 @@ source("Helpers.R")
 
 # Load Data
 load("./house_imputed.RData") # Loads houses.train and houses.test
+houses.train <- as.data.frame(houses.train)
+
+##################
+# Encoding
+##################
 
 # Take log of house price label
 houses.train$SalePrice <- log(houses.train$SalePrice + 1)
 
-plot(model.baseline)
-
-# RMSE of model on our test split
-private.test[which(private.test$Condition2 %in% c("PosA", "RRNn")),]$Condition2 <- "Norm"
-private.test[which(private.test$Exterior1st %in% c("ImStucc")),]$Exterior1st <- "VinylSd"
+# private.test[which(private.test$Condition2 %in% c("PosA", "RRNn")),]$Condition2 <- "Norm"
+# private.test[which(private.test$Exterior1st %in% c("ImStucc")),]$Exterior1st <- "VinylSd"
 ## Not sure how to handle missing factor levels in test set
 
-#Create Dummy Variables (full training data)
-# encode using caret::dummyVars
-features.to.encode <- c("MSZoning", "OverallQual", "OverallCond", "ExterCond") # test with a few variables
-f <- paste("~", paste(features.to.encode, collapse="+"))
-encoder <- caret::dummyVars(as.formula(f),
-                            data = houses.train,
-                            sep = ".",
-                            fullRank = FALSE) # check meaning of this. Includes no interactions between variables e.g. feature1:feature2
-encoded.houses.train <- predict(encoder, houses.train)
+# Scale numeric features
+indices <- sapply(houses.train, is.numeric)
+indices["SalePrice"] <- FALSE # don't scale the labels
+houses.train[indices]  <- data.frame(lapply(houses.train[indices], scale))
 
-encoded.houses.train <- cbind(as.data.frame(encoded.houses.train), houses.train$SalePrice)
-# rename imported column 
-colnames(encoded.houses.train)[which(names(encoded.houses.train) == "houses.train$SalePrice")] <- "SalePrice"
+# One-hot encoding of categorical features
+# using vtreat over caret::dummyVars because handles new levels in test set
+set.seed(0)
+vtreat_encoder <- vtreat::designTreatmentsZ(houses.train, 
+                                   colnames(houses.train), 
+                                   minFraction= 0,
+                                   verbose=FALSE)
+
+# restrict to common variables types
+# see vignette('vtreatVariableTypes', package = 'vtreat') for details
+sf <- vtreat_encoder$scoreFrame
+newvars <- sf$varName[sf$code %in% c("lev", "clean", "isBAD")] 
+houses.train.Vtreat <- as.data.frame(vtreat::prepare(vtreat_encoder, 
+                                         houses.train, 
+                                         varRestriction = newvars))
+# Save encoded data.frame
+save(houses.train.Vtreat, file = "./house_encoded.RData")
+
+##################
+# Run Linear Model
+##################
+
 
 # Split our data into a train and test set - 80/20
 set.seed(0)
 split_ratio = 0.8
-train = sample(1:nrow(houses.train), nrow(encoded.houses.train)*split_ratio)
+train = sample(1:nrow(houses.train.Vtreat), nrow(houses.train.Vtreat)*split_ratio)
 
-encoded.private.train = encoded.houses.train[train,]
-encoded.private.test = encoded.houses.train[-train,]
+private.train = houses.train.Vtreat[train,]
+private.test = houses.train.Vtreat[-train,]
+
 
 # Baseline linear model
-model.baseline <- lm(SalePrice ~ ., data=encoded.private.train)
-predicted <- predict(model.baseline, encoded.private.test, na.action = na.exclude) 
-actual <- encoded.private.test$SalePrice
+model.baseline <- lm(SalePrice_clean ~ ., data=private.train)
+#plot(model.baseline) # review assumptions
+predicted <- predict(model.baseline, private.test, na.action = na.exclude) 
+actual <- private.test$SalePrice
 
 sqrt(mean((predicted-actual)^2))
+
+
+
+
+
+
