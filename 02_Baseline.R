a08e2b8dead8096b91a6564af059d603c25a9f58
Spugnam <quentin.picard@gmail.com>
2017-11-04 17:50:12 -0400

    corrected Baseline

--- 02_Baseline.R
+++ 02_Baseline.R
@@ -8,85 +8,84 @@ library(vtreat)
 # Load Helper Functions
 source("Helpers.R")
 
-# Load Data
-load("./house_imputed.RData") # Loads houses.train and houses.test
-houses.train <- as.data.frame(houses.train)
-houses.test <- as.data.frame(houses.test)
+##################
+# Load
+##################
 
-# Take log of house price label
-houses.train$SalePrice <- log(houses.train$SalePrice + 1)
-houses.test$SalePrice <- log(houses.test$SalePrice + 1)
+load("./house_imputed.RData") 
+class(houses.train) # "data.table" "data.frame"
+class(houses.test) # "data.table" "data.frame"
 
-houses <- rbind(houses.train, houses.test)
+#########################
+# Create private test set
+#########################
+
+# Split our data into a train and test set - 80/20
+set.seed(0)
+split.ratio = 0.8
+train.indices = sample(1:nrow(houses.train), nrow(houses.train)*split.ratio)
+private.train = houses.train[train.indices,] # dim: 1168, 80
+private.test = houses.train[-train.indices,] # dim: 292, 80
 
 ##################
 # Encoding
 ##################
 
-# Scale numeric features
-indices <- sapply(houses.train, is.numeric)
-indices["SalePrice"] <- FALSE # don't scale the labels
-houses.train[indices]  <- data.frame(lapply(houses.train[indices], scale))
+# Convert to data.frame (from data.table) to avoid bugs below
+private.train <- as.data.frame(private.train)
+private.test <- as.data.frame(private.test)
+houses.train <- as.data.frame(houses.train)
+houses.test <- as.data.frame(houses.test)
 
-indices <- sapply(houses, is.numeric)
-indices["SalePrice"] <- FALSE # don't scale the labels
-houses[indices]  <- data.frame(lapply(houses[indices], scale))
+# Take log of house price label
+private.train$SalePrice <- log(private.train$SalePrice + 1)
+private.test$SalePrice <- log(private.test$SalePrice + 1)
+houses.train$SalePrice <- log(houses.train$SalePrice + 1)
+houses.test$SalePrice <- log(houses.test$SalePrice + 1)
 
+# One-hot encode categorical features using vtreat
+# Scale all features including dummy ones per: https://stats.stackexchange.com/questions/69568/whether-to-rescale-indicator-binary-dummy-predictors-for-lasso
+encoded.private.train <- encode.scale.df(private.train[ , -which(names(private.train) == "SalePrice")])
+encoded.private.train['SalePrice'] <- private.train$SalePrice
 
+encoded.private.test <- encode.scale.df(private.test[ , -which(names(private.test) == "SalePrice")])
+encoded.private.test['SalePrice'] <- 0
 
-# One-hot encoding of categorical features
-# using vtreat over caret::dummyVars because handles new levels in test set
-set.seed(0)
+encoded.houses.train <- encode.scale.df(houses.train[ , -which(names(houses.train) == "SalePrice")])
+encoded.houses.train['SalePrice'] <- houses.train$SalePrice
 
-vtreat_encoder_train <- vtreat::designTreatmentsZ(houses.train,
-                                            colnames(houses.train),
-                                            minFraction= 0,
-                                            verbose=FALSE)
+encoded.houses.test <- encode.scale.df(houses.test[ , -which(names(houses.test) == "SalePrice")])
+encoded.houses.test['SalePrice'] <- 0
 
-vtreat_encoder_full <- vtreat::designTreatmentsZ(houses, 
-                                                 colnames(houses), 
-                                                 minFraction= 0,
-                                                 verbose=FALSE)
+# Add missing columns in test set with default value equal to 0
+# This approach was chosen over binding training and test because it would leak information from test
+# Also remove columns from test not in training (no predictive value)
+encoded.private.test <- align.columns(encoded.private.train, encoded.private.test)
 
-# One-hot Encoding with vtreat
-# see vignette('vtreatVariableTypes', package = 'vtreat') for details
-sf <- vtreat_encoder_train$scoreFrame
-newvars <- sf$varName[sf$code %in% c("lev", "clean", "isBAD")] # TO DO check how this works
-houses.train.Vtreat <- as.data.frame(vtreat::prepare(vtreat_encoder_train,
-                                                     houses.train,
-                                                     varRestriction = newvars))
-
-sf <- vtreat_encoder_full$scoreFrame
-newvars <- sf$varName[sf$code %in% c("lev", "clean", "isBAD")] # TO DO check how this works
-houses.Vtreat <- as.data.frame(vtreat::prepare(vtreat_encoder_full, 
-                                                    houses, 
-                                                    varRestriction = newvars))
+encoded.houses.test <- align.columns(encoded.houses.train, encoded.houses.test)
 
 
-# Save encoded data.frame
-save(houses.Vtreat, file = "./house_encoded.RData")
+# Save encoded dataframes
+# save(encoded.private.train, file = "./encoded.private.train.RData")
+# save(encoded.private.test, file = "./encoded.private.test.RData")
+# save(encoded.houses.train, file = "./encoded.houses.train.RData")
+# save(encoded.houses.test, file = "./encoded.houses.test.RData")
 
-##################
-# Run Linear Model
-##################
+#######################
+# Baseline Linear Model
+#######################
 
-
-# Split our data into a train and test set - 80/20
-set.seed(0)
-split_ratio = 0.8
-train = sample(1:nrow(houses.train.Vtreat), nrow(houses.train.Vtreat)*split_ratio)
-private.train = houses.train.Vtreat[train,]
-private.test = houses.train.Vtreat[-train,]
 
 # Baseline linear model
-# model.baseline <- lm(SalePrice_clean ~ ., data=private.train)
-# summary(model.baseline)
-
+model.baseline <- lm(SalePrice ~ ., data=encoded.private.train)
+summary(model.baseline)
 #plot(model.baseline) # review assumptions
-# predicted <- predict(model.baseline, private.test, na.action = na.exclude) 
-# actual <-private.test$SalePrice_clean
-# 
-# sqrt(mean((predicted-actual)^2))
+
+predicted <- predict(model.baseline, encoded.private.test, na.action = na.exclude) 
+actual <-private.test$SalePrice
+
+# RMSE
+sqrt(mean((predicted-actual)^2))
 
 ##################
 # Regularization
@@ -99,24 +98,27 @@ grid_alpha = seq(0, 1, length=21)
 
 # Cross-Validation for alpha & lambda
 set.seed(1000)
-train_control = trainControl(method = 'cv', number=10)
+train.control = trainControl(method = 'cv', number=10)
 tune.grid = expand.grid(lambda = grid_lambda, alpha=grid_alpha)
-ridge.caret = train(private.train, 
-                    private.train$SalePrice_clean,
+ridge.caret = train(encoded.private.train[ , -which(names(encoded.private.train) == "SalePrice")], 
+                    encoded.private.train$SalePrice,
                     method = 'glmnet',
-                    trControl = train_control, 
+                    trControl = train.control, 
                     tuneGrid = tune.grid)
 
 alpha <- ridge.caret$bestTune$alpha
 lambda <- ridge.caret$bestTune$lambda
 
-tuned.linear.pred = predict(ridge.caret, newdata = private.test)
-sqrt(mean((tuned.linear.pred - private.test$SalePrice_clean)^2))
+tuned.linear.pred = predict(ridge.caret, 
+                            newdata = encoded.private.test[ , -which(names(encoded.private.test) == "SalePrice")])
+# RMSE
+sqrt(mean((tuned.linear.pred - private.test$SalePrice)^2))
 
 # Retraining on full training set
-x = model.matrix(SalePrice_clean ~ ., houses.Vtreat[1:1460,])
-y = houses.Vtreat[1:1460,]$SalePrice_clean
-z = model.matrix(SalePrice_clean~ ., houses.Vtreat[1461:2919,])
+
+x = model.matrix(SalePrice ~ ., encoded.houses.train)
+y = encoded.houses.train$SalePrice
+z = model.matrix(SalePrice ~ ., encoded.houses.test)
 
 ridge.caret.full.training = glmnet(x, y, 
                                    alpha = alpha, 
--- Helpers.R
+++ Helpers.R
@@ -34,6 +34,45 @@ impute.mode <- function(DT, column){
   return(DT[[column]])
 }
 
+##########################
+# Encoding functions
+##########################
+
+encode.scale.df <- function(df){
+  # Creates dummy variables and scale the whole dataframe
+  
+  set.seed(0)
+  vtreat.encoder.df <- vtreat::designTreatmentsZ(df,
+                                                            colnames(df),
+                                                            minFraction= 0,
+                                                            verbose=FALSE) # creates a "treatmentplan"
+  
+  # see vignette('vtreatVariableTypes', package = 'vtreat') for details
+  sf <- vtreat.encoder.df$scoreFrame
+  newvars <- sf$varName[sf$code %in% c("lev", "clean", "isBAD")] 
+  encoded.df <- as.data.frame(vtreat::prepare(vtreat.encoder.df,
+                                              df,
+                                              scale = FALSE,  # doesn't work with TRUE
+                                              varRestriction = newvars))
+  cat("Encoded dataframe dimensions: ", dim(encoded.df))
+  data.frame(lapply(encoded.df, scale))
+}
+
+# Code to scale numeric features only
+# indices <- sapply(houses, is.numeric)
+# indices["SalePrice"] <- FALSE # don't scale the labels
+# houses[indices]  <- data.frame(lapply(houses[indices], scale))
+
+align.columns <- function(training.df, test.df) {
+  # Add columns in training.df not in test.df to test.df, filled with 0s
+  # Remove test.df columns not in training.df
+  missing.cols <- setdiff(names(training.df), names(test.df))
+  test.df[missing.cols] <- 0                   
+  
+  # Ensure same the order of columns in the training and test sets
+  test.df[names(training.df)] 
+}
+
 
 
 
