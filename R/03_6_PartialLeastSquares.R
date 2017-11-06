ccf2d1f9699303964cefe1b1dcdea4fdba63e08d
= <paul.ton@outlook.com>
2017-11-06 01:11:51 -0500

    update pls

--- R/03_6_PartialLeastSquares.R
+++ R/03_6_PartialLeastSquares.R
@@ -2,12 +2,15 @@ library(MASS)
 library(car)
 library(caret)
 
-model.baseline <- lm(SalePrice_clean ~ ., data=private.train)
+load("./data/encoded.private.train.RData")
+load("./data/encoded.private.test.RData")
+
+model.baseline <- lm(SalePrice ~ ., data=encoded.private.train)
 bc <- boxCox(model.baseline)
 lambda = bc$x[which(bc$y == max(bc$y))]
 
-x <- subset(private.train, select=-c(SalePrice_clean))
-y <- (private.train$SalePrice_clean^lambda - 1)/lambda
+x <- subset(encoded.private.train, select=-c(SalePrice))
+y <- (encoded.private.train$SalePrice^lambda - 1)/lambda
 
 set.seed(100)
 
@@ -36,12 +39,12 @@ model.pls <- train(x, y, method = "pls", tuneLength = 200,
 
 pred = predict(model.pls, newdata = x)
 pred = log(unbox(pred, lambda))
-actual <- log(private.train$SalePrice_clean)
+actual <- log(encoded.private.train$SalePrice)
 
 sqrt(mean((pred-actual)^2))
 
-pred = predict(model.pls, newdata = private.test)
+pred = predict(model.pls, newdata = encoded.private.test)
 pred = log(unbox(pred, lambda))
-actual <- log(private.test$SalePrice_clean)
+actual <- log(private.test$SalePrice)
 
 sqrt(mean((pred-actual)^2))
