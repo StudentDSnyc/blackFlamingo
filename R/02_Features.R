dbdca8ae379cc269f262a7c789ead05caf643fb0
Spugnam <quentin.picard@gmail.com>
2017-11-06 09:23:12 -0500

    added 2 interactions in 02_Feature.R

--- R/02_Features.R
+++ R/02_Features.R
@@ -1,3 +1,8 @@
+library(devtools)
+#install_github("gathanei/xyz")
+library("xyz")
+
+
 ##################
 # Load
 ##################
@@ -6,7 +11,7 @@ load("./house_imputed.RData")
 class(houses.train) # "data.table" "data.frame"
 class(houses.test) # "data.table" "data.frame"
 
-houses = rbind(houses.train, houses.test) # need to only do feature engineering that doesn't lead information
+houses = rbind(houses.train, houses.test) # need to only do feature engineering that doesn't leak information
 
 # Living Area http://homeguides.sfgate.com/calculate-living-area-square-footage-33755.html 
 houses[, c("Adj.GrLivArea") := list(BsmtFinSF1+BsmtFinSF2+GrLivArea)]
@@ -14,6 +19,33 @@ houses[, c("Adj.GrLivArea") := list(BsmtFinSF1+BsmtFinSF2+GrLivArea)]
 # Number of bathrooms. Only count basement bathrooms if there's a 'finished' basement area
 houses[BsmtFinSF1>0, c("TotalBath") := list(FullBath + .5*HalfBath + BsmtFullBath + .5*BsmtHalfBath)]
 houses[BsmtFinSF1==00, TotalBath := list(FullBath + .5*HalfBath)]
+
+# Interactions # Prints interaction but not sure how to create the features we need or get model using them
+result <- xyz_regression(data.matrix(houses[1:1460, -which(names(houses) == "SalePrice"), with=FALSE]),
+               houses[1:1460, ][["SalePrice"]], # using list subsetting '[[' to access data.table column (fast)
+               lambdas = 10^seq(1, -3, length = 10), n_lambda = 10,
+               alpha = 0.1, L = 10,
+               standardize = TRUE,
+               standardize_response = TRUE)
+
+# Interaction effect:
+# Interaction effect: (26,72) coefficient: 0.2037225 # MasVnrArea PoolQC
+# Interaction effect: (14,28) coefficient: -0.1285461 # Condition2 ExterCond    # ok maybe
+# Interaction effect: (14,38) coefficient: -0.1203811 # Condition2 TotalBsmtSF ??
+# Interaction effect: (14,67) coefficient: -0.1122927 # Condition2 OpenPorchSF
+# Interaction effect: (4,8) coefficient: -0.1066311 # LotArea LandContour       # ok makes sense
+# Interaction effect: (38,72) coefficient: 0.08450363 # TotalBsmtSF PoolQC
+
+# Interaction effect: (14,14) coefficient: -0.08428927
+# Interaction effect: (30,72) coefficient: 0.07212801
+# Interaction effect: (4,4) coefficient: -0.0693587
+# Interaction effect: (3,26) coefficient: 0.06451609
+# Interaction effect: (3,43) coefficient: -0.06385154
+
+# Variable for MasVnrArea/ PoolQC effect? doesn't make sense (only 3 rows applicable)
+# houses[, c("PoolMasVnrArea.interaction") := list(with(houses, interaction(quantile(houses$MasVnrArea, probs = seq(0, 1, 0.05)),  PoolQC)))]
+houses[, c("Condition2.ExterCond.interaction") := list(with(houses, interaction(Condition2,  ExterCond)))]
+houses[, c("LotArea.LandContour.interaction") := list(with(houses, interaction(quantile(houses$LotArea, probs = seq(0, 1, 0.05)),  LandContour)))]
 
 # Put SalePrice back in the last position
 setcolorder(houses, c(setdiff(names(houses), "SalePrice"), "SalePrice"))
--- R/03_0_Baseline.R
+++ R/03_0_Baseline.R
@@ -13,7 +13,9 @@ source("Helpers.R")
 # Load
 ##################
 
-load("./house_imputed.RData") 
+# load("./house_imputed.RData") 
+load("./houses.train.RData")
+load("./houses.test.RData")
 class(houses.train) # "data.table" "data.frame"
 class(houses.test) # "data.table" "data.frame"
 
@@ -81,7 +83,7 @@ bc.lambda = bc$x[which(bc$y == max(bc$y))]
 x <- encoded.private.train %>% select(-SalePrice)
 y <- (encoded.private.train$SalePrice^bc.lambda - 1)/bc.lambda
 
-model.bc <- lm(y ~ ., data=x)
+model.bc <- lm(y ~ . , data=x)
 
 predicted <- predict(model.bc, encoded.private.test, na.action = na.exclude) 
 predicted <- log(unbox(predicted, bc.lambda))
@@ -95,8 +97,8 @@ sqrt(mean((predicted-actual)^2))
 ##################
 
 #Values of lambda over which to check.
-grid_lambda = 10^seq(4, -3, length = 20)
-# plot(1:20, grid_lambda)
+grid_lambda = 10^seq(1, -3, length = 20)
+#plot(1:20, grid_lambda)
 grid_alpha = seq(0, 1, length=21)
 
 # Cross-Validation for alpha & lambda
@@ -104,8 +106,44 @@ set.seed(1000)
 train.control = trainControl(method = 'cv', number=10, verboseIter = TRUE)
 tune.grid = expand.grid(lambda = grid_lambda, alpha=grid_alpha)
 
+# test add interactions
+# library("xyz")
+# result <- xyz_regression(data.matrix(encoded.private.train[ , -which(names(encoded.private.train) == "SalePrice")]),
+#                          as.vector((encoded.private.train$SalePrice^bc.lambda - 1)/bc.lambda),
+#                          lambdas = 10^seq(1, -3, length = 10), n_lambda = 10,
+#                          alpha = 0.1, L = 10,
+#                          standardize = TRUE,
+#                          standardize_response = TRUE)
+# Model parameters:
+#   intercept: -6.193536e-16
+# Printing effects for lambda10=0.001
+# Main effects:
+#   Main effect: 231 coefficient: 0.3058002 # GrLivArea_clean
+# Main effect: 118 coefficient: 0.1289097 # YearBuilt_clean
+# Main effect: 22 coefficient: 0.1256254 # LotArea_clean
+# Main effect: 215 coefficient: 0.1177676 # TotalBsmtSF_clean
+# Main effect: 322 coefficient: 0.06945074 # SaleCondition_lev_x.Partial
+# Interaction effect: 
+# Reduced from before one-hot encoding
+# Interaction effect: (35,45) coefficient: 0.04940778 # "LandContour_lev_x.Lvl" "LandSlope_lev_x.Sev"
+# Interaction effect: (67,322) coefficient: 0.04885172 # "Neighborhood_lev_x.StoneBr"  "SaleCondition_lev_x.Partial"
+# Interaction effect: (21,22) coefficient: -0.04836661 # "LotFrontage_clean" "LotArea_clean"
+# Interaction effect: (238,240) coefficient: -0.04272843 # "KitchenQual_lev_x.Ex" "KitchenQual_lev_x.Gd"
+# Interaction effect: (16,228) coefficient: 0.04085933 # "MSZoning_lev_x.C..all." "CentralAir_lev_x.Y" 
+
+# f <- as.formula( ~ .*.) 
+# x <- model.matrix(f, encoded.private.train[ , -which(names(encoded.private.train) == "SalePrice")])[, -1] 
+# 
+# glmnet.caret = train(x, 
+#                      (encoded.private.train$SalePrice^bc.lambda - 1)/bc.lambda,
+#                      method = 'glmnet',
+#                      trControl = train.control, 
+#                      tuneGrid = tune.grid)
+
+# end test variations
+
 glmnet.caret = train(encoded.private.train[ , -which(names(encoded.private.train) == "SalePrice")], 
-                    (encoded.private.train$SalePrice^lambda - 1)/lambda,
+                    (encoded.private.train$SalePrice^bc.lambda - 1)/bc.lambda,
                     method = 'glmnet',
                     trControl = train.control, 
                     tuneGrid = tune.grid)
@@ -113,8 +151,6 @@ glmnet.caret = train(encoded.private.train[ , -which(names(encoded.private.train
 alpha <- glmnet.caret$bestTune$alpha
 lambda <- glmnet.caret$bestTune$lambda
 
-tuned.linear.pred = predict(glmnet.caret, 
-                            newdata = encoded.private.test[ , -which(names(encoded.private.test) == "SalePrice")])
 # RMSE
 sqrt(mean((log(unbox(tuned.linear.pred, bc.lambda)) - log(private.test$SalePrice))^2))
 
@@ -130,6 +166,10 @@ glmnet.caret.full.training = glmnet(x, y,
 
 linear.pred.full.training = predict(glmnet.caret.full.training, 
                                     newx = z)
+
+# Test quadratic
+# f <- as.formula(paste0("SalePrice ~ poly(", paste0(colnames(encoded.private.train[ , -which(names(encoded.private.train) == "SalePrice")]), collapse = ' + '), ", 2)"))
+
 
 # Create submission file
 write.csv(data.frame(Id = 1461:2919, SalePrice = unbox(linear.pred.full.training[,1])), 
